c7d3a4957567fe69b0f40e6c2f52d8d710aa97ff
unknown <tushartushid01@gmail.com>
2024-01-29 19:31:19 -0400

    modified main

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,29 @@
 package cmd
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	databaseName := os.Getenv("DB_NAME")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	serverPort := os.Getenv("SERVER_PORT")
+	err := database.ConnectAndMigrate(host, port, databaseName, user, password, database.SSLModeDisable)
+	if err != nil {
+		logrus.Printf("ConnectAndMigrate: error is:%v", err)
+		return
+	}
+	fmt.Println("connected")
+	srv := server.SetupRoutes()
+	go cron.RunCronJob()
+	err = srv.Run(fmt.Sprintf(":%s", serverPort))
+	if err != nil {
+		logrus.Printf("could not run the server:%v", err)
+		return
+	}
+	// log.Printf("check 101 for main")
+}
